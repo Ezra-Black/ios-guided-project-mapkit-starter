43535a37e5f73fbf4cdad8a0c290e1c4dc73ac39
Ezra Black <dev.ezrablack@gmail.com>
2020-05-07 17:07:37 -0500

    Added Custom Key decoding that can decode desired data model/

--- Quakes/Quakes/Quake.swift
+++ Quakes/Quakes/Quake.swift
@@ -14,6 +14,14 @@ class Quake: NSObject, Decodable {
     
     enum CodingKeys: String, CodingKey {
         case magnitude = "mag"
+        case place
+        case time
+        case latitude
+        case longitude
+        
+        case properties
+        case geometry
+        case coordinates
     }
     let magnitude: Double
     let place: String
@@ -24,13 +32,17 @@ class Quake: NSObject, Decodable {
     required init(from decoder: Decoder)
         throws {
             //Containers
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            let properties = try container.nestedContainer(keyedBy: CodingKeys.self, forKey: .properties)
+            let geometry = try container.nestedContainer(keyedBy: CodingKeys.self, forKey: .geometry)
+            var coordinates = try geometry.nestedUnkeyedContainer(forKey: .coordinates)
             
             //set each value here
-            self.magnitude = 0
-            self.place = ""
-            self.time = Date()
-            self.latitude = 0
-            self.longitude = 0
+            self.magnitude = try properties.decode(Double.self, forKey: .magnitude)
+            self.place = try properties.decode(String.self, forKey: .place)
+            self.time = try properties.decode(Date.self, forKey: .time)
+            self.longitude = try coordinates.decode(Double.self)
+            self.latitude = try coordinates.decode(Double.self)
             
             super.init()
     }
